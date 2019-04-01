cb29529a95087f2b8bcc4c5e061bd0f95b9b6f39
CamilBouzidi <45476178+CamilBouzidi@users.noreply.github.com>
2019-04-01 18:12:14 -0400

    Started Txt Parsing
    It's not done, but Question 1 is getting there!

--- Assignment 4/src/Assignment4Question1.java
+++ Assignment 4/src/Assignment4Question1.java
@@ -14,18 +14,45 @@ public class Assignment4Question1 {
 		System.out.println("Welcome to the Sub-dictionary Creator!"
 				+ "\n Please enter the name of the .txt file you would like to go through: ");
 		nameIn = kb.next();
+		ArrayList<String> wordList = new ArrayList<String>();
+		ArrayList<String> illegal = new ArrayList<String>();
+		illegal.add(".");
+		illegal.add(",");
+		illegal.add("!");
+		illegal.add("?");
+		illegal.add(";");
+		illegal.add(":");
+		illegal.add("=");
+		
 		try {
 			sc = new Scanner(new FileInputStream(nameIn+".txt"));
 			pw = new PrintWriter(new FileOutputStream("SubDictionary.txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 			System.out.println("Program will terminate.");
+			sc.close();
+			pw.close();
 			System.exit(0);
 		} finally {
 			while (sc.hasNext()) {
 				String word = sc.next();
+				word = word.toUpperCase();
+				if (!wordList.contains(word)) {
+				for (String p : illegal) {
+					if (word.contains(p)) {
+					System.out.println("Error: "+p+" found in "+word);
+					word = word.substring(0, word.length()-1);
+					System.out.println("The word is now: "+word);
+					}
+				}
+				wordList.add(word);
+				pw.println(word);
+				}
 			}
+			pw.close();
 		}
+		for(String c: wordList)
+			System.out.println(c);
 	}
 
 }
